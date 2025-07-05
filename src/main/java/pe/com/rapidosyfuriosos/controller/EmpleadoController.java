03740afbf0abd6433fd6084c9b1ad33f503a22e6
Guillermo <guille.anticona@gmail.com>
2025-07-04 20:59:48 -0500

    crud empleado

--- src/main/java/pe/com/rapidosyfuriosos/controller/EmpleadoController.java
+++ src/main/java/pe/com/rapidosyfuriosos/controller/EmpleadoController.java
@@ -3,16 +3,30 @@ package pe.com.rapidosyfuriosos.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pe.com.rapidosyfuriosos.entity.EmpleadoEntity;
+import pe.com.rapidosyfuriosos.service.DistritoService;
 import pe.com.rapidosyfuriosos.service.EmpleadoService;
+import pe.com.rapidosyfuriosos.service.RolService;
+import pe.com.rapidosyfuriosos.service.SexoService;
 
-@RestController
+@Controller
 @RequestMapping("/empleado")
 public class EmpleadoController {
 
     @Autowired
     private EmpleadoService service;
+
+    @Autowired
+    private DistritoService distritoService;
+
+    @Autowired
+    private RolService rolService;
+
+    @Autowired
+    private SexoService sexoService;
 
     @GetMapping
     public List<EmpleadoEntity> listar() {
@@ -43,4 +57,51 @@ public class EmpleadoController {
     public EmpleadoEntity habilitar(@PathVariable Long id) {
         return service.enable(id);
     }
+
+    @GetMapping("/vista")
+    public String listarVista(Model model) {
+        model.addAttribute("empleados", service.findAll());
+        return "empleado/listar_empleado";
+    }
+
+    @GetMapping("/registrar")
+    public String mostrarFormularioRegistro(Model model) {
+        model.addAttribute("empleado", new EmpleadoEntity());
+        model.addAttribute("distritos", distritoService.findAll());
+        model.addAttribute("roles", rolService.findAll());
+        model.addAttribute("sexos", sexoService.findAll());
+        return "empleado/registrar_empleado";
+    }
+
+    @PostMapping("/registrar")
+    public String registrarEmpleado(@ModelAttribute("empleado") EmpleadoEntity empleado) {
+        service.add(empleado);
+        return "redirect:/empleado/vista";
+    }
+
+    @GetMapping("/actualizar/{id}")
+    public String mostrarFormularioActualizar(@PathVariable Long id, Model model) {
+        EmpleadoEntity empleado = service.findById(id);
+        model.addAttribute("empleado", empleado);
+        model.addAttribute("distritos", distritoService.findAll());
+        model.addAttribute("roles", rolService.findAll());
+        model.addAttribute("sexos", sexoService.findAll());
+        return "empleado/actualizar_empleado";
+    }
+
+    @PostMapping("/actualizar")
+    public String actualizarEmpleado(@ModelAttribute("empleado") EmpleadoEntity empleado) {
+        service.update(empleado, empleado.getCodigo());
+        return "redirect:/empleado/vista";
+    }
+
+    @GetMapping("/habilitar/{id}")
+    public String habilitarOInhabilitar(@PathVariable Long id) {
+        service.enable(id);
+        return "redirect:/empleado/vista";
+    }
+
+
+
+
 }
